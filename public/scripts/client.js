4941a47a6feafc3520d75bf142a09acd92573ecf
Amy Venturino <aventuri@umich.edu>
2017-04-30 16:11:53 -0500

    Appending with status and delete options

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,7 +2,9 @@ $(document).ready(onReady);
 
 function onReady (){
   console.log('script sourced');
+  getAllTasks();
   $('#addTaskButton').on('click', addTask);
+  $('.taskList').on('click', deleteTask);
 }
 
 function addTask (){
@@ -31,6 +33,24 @@ function getAllTasks (){
     type: 'GET',
     success: function ( response ){
       console.log('back from server with:', response);
+      appendTasks(response);
     }//end successs
   });//end GET
 }//end get all Tasks
+
+
+function appendTasks(array) {
+  $( '.taskList' ).empty();
+     for (var i = 0; i < array.length; i++) {
+       if (array[i].complete === false){
+       $( '.taskList' ).append( '<div class="incompleteTask" id="' + array[i].id +' "><p>' + array[i].task_name + ' ' + '<button id="complete">Complete</button>' + '<button id="delete">Delete</button>' + '</p></div>' );
+        } else {
+          $( '.taskList' ).append( '<div class="completedTask" id="' + array[i].id +' "><p>' + array[i].task_name + ' ' + '<button id="delete">Delete</button>' + '</p></div>' );
+           }
+        }//end for loop
+}//end appendTasks
+
+function deleteTask () {
+  var id = $(this).parent().attr('id');
+  console.log( $(this).parent().attr('id'));
+}//end delete Task
